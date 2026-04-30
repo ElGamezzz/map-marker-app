48a861036b3c5d718fc5caef95189fb3b85bf426
José Sebastián Delgado Gamez <142054398+ElGamezzz@users.noreply.github.com>
2026-04-29 19:05:30 -0500

    chore: init spring boot project with JPA & H2

---
+++ src/main/java/com/ElGamezzz/map_marker_app/MapMarkerAppApplication.java
@@ -0,0 +1,13 @@
+package com.ElGamezzz.map_marker_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapMarkerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapMarkerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ElGamezzz/map_marker_app/MapMarkerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ElGamezzz.map_marker_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapMarkerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
